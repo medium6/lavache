b332e5be4ca52ca1aaeadf85003737dd1c74cf10
medium6 <rrabbit59@yahoo.fr>
2020-06-15 17:10:01 +0200

    first deployment of lavache django project

---
+++ blog/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from .models import Post
+# Create your views here.
+
+def showArticles(request):
+    articles = Post.objects.all()
+    return render(request, 'blog/articles.html', {'articles':articles})
